cab17f4a3ee3545a89e041609c8adb4f13ab341e
Aleksandr Kuplays <alexkuplays@gmail.com>
2018-12-07 01:13:23 +0700

    Server so far

--- Server_GRADLE/src/main/java/ru/sibguti/Server/MyThread.java
+++ Server_GRADLE/src/main/java/ru/sibguti/Server/MyThread.java
@@ -2,6 +2,8 @@ package ru.sibguti.Server;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.FileInputStream;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -9,11 +11,52 @@ public class MyThread implements Runnable {
 	private Socket socketClient;
 	private InputStream inputStream;
 	private OutputStream outputStream;
+	private Scanner addressLineScanner, fileScanner;
+	private FileInputStream fileStream;
+	private PrintWriter outWriter;
+	String address, addrFromStream[];
+	String content, responseHeader;
+	
 	
 	public MyThread(Socket sock) {
-		
+		this.socketClient = sock;
+		address = "";
+		content = "";
+	//	responseHeader = "HTTP/1.1 OK 200\r\nContent-Type: text/html\r\nContent-Length: 128\r\n";
+		try {
+			this.inputStream = this.socketClient.getInputStream();
+			this.outputStream = this.socketClient.getOutputStream();
+		} catch(Exception ex) {
+			ex.printStackTrace();
+		}	
 	}
 
 	public void run() {
+		addressLineScanner = new Scanner(inputStream);
+		addrFromStream = addressLineScanner.nextLine().split(" ");
+		
+		if (addrFromStream[1].equals("/")) address = "index.html";
+		else address = addrFromStream[1].substring(1, addrFromStream[1].length());
+		
+		try {
+			fileStream = new FileInputStream(address);
+			fileScanner = new Scanner(fileStream);
+			
+			while(fileScanner.hasNextLine())
+				content += fileScanner.nextLine();
+			fileStream.close();
+			
+		} catch(Exception ex) {
+			ex.printStackTrace();
+		}
+		
+		responseHeader = "HTTP/1.1 OK 200\r\nContent-Type: text/html\r\nContent-Length: " + String.valueOf(content.length()) + "\r\n";
+		String fullResponse = responseHeader + content;
+		outWriter = new PrintWriter(outputStream, true);
+		outWriter.println(fullResponse);
+		System.out.println(fullResponse);
+	//	outWriter.flush();
+		address = "";
+		content = "";
 	}
 }
--- Server_GRADLE/src/main/java/ru/sibguti/Server/Runner.java
+++ Server_GRADLE/src/main/java/ru/sibguti/Server/Runner.java
@@ -1,7 +1,36 @@
 package ru.sibguti.Server;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.*;
+
 public class Runner {
 	public static void main(String argv[]) {
-		System.out.println("SUCCESS");
+//		try {
+//		ServerSocket serv = new ServerSocket(8080);
+//		Socket cs = serv.accept();
+//		PrintWriter out = new PrintWriter(cs.getOutputStream(), true);
+//		BufferedReader in = new BufferedReader(new InputStreamReader(cs.getInputStream()));
+//		out.println("<html><body>gggg</body></html>");
+//		out.close();
+//		serv.close();
+//		} catch(Exception e) {}
+
+//		ServerSocket server;
+///		Socket client;
+//		Runnable runnable;
+//		Thread thread;
+
+		try {
+			ServerSocket server = new ServerSocket(8080);
+			while(true) {
+				Socket client = server.accept();
+				Runnable runnable = new MyThread(client);
+				Thread thread = new Thread(runnable);
+				thread.start();
+			}
+		} catch(Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 }
